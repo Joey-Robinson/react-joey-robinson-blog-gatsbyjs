da774363ce486759b80a00d98351d3860e497e33
Joey Robinson <theamazingness@gmail.com>
2019-04-30 00:05:39 -0400

    Not sure where I left off

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import { FaLinkedin } from 'react-icons/fa/index.esm'
 import { FaGithub } from 'react-icons/fa/index.esm'
 import { MdEmail } from 'react-icons/md/index.esm'
 import { FaTwitterSquare } from 'react-icons/fa/index.esm'
+import AniLink from "gatsby-plugin-transition-link/AniLink"
 
 const Home = () => (
   <Layout>
@@ -13,7 +14,8 @@ const Home = () => (
     <section className="home">
       <div className="home--content__top">
         <h1>Hi, I'm Joey Robinson</h1>
-        <p>I'm a Frontend Developer and I like making things. My current primary focus is using React (GatsbyJS) and Vanilla JavaScript. I'm not a designer but I enjoy working with S/CSS and CSS Grid. I'm looking to create and maintain amazing websites and web applications. Feel free to contact me with any questions or comments.</p>
+        <p>I'm a Frontend Developer and I like making things. My primary focus is using React (GatsbyJS) and Vanilla JavaScript. I'm not a designer but I enjoy working with S/CSS and CSS Grid. I'm looking to create and maintain amazing websites and web applications. Feel free to contact me with any questions or comments.</p>
+        <p>A bit more about me: I started web development in 2017 and have been teaching myself everything I can inside of the development sphere. To read more about me head over to the <AniLink direction="right" cover bg="#05012b" to="/about/">About</AniLink> page.</p>
       </div>
       <ul className="home--content__bottom">
         <li>
