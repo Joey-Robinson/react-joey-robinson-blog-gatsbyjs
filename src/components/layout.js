6c781467d5d4d204386d19eeece046ea906a0cd0
Joey Robinson <theamazingness@gmail.com>
2019-06-25 13:50:07 -0400

    Added required packages and libraries

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,6 @@
 import React from "react"
 import Header from "./header"
+import "./index.css"
 
 const Layout = ({ children }) => {
   return (
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import DarkModeToggle from "../components/dark-mode"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Currently updating this. Check back often</h1>
+    <DarkModeToggle />
   </Layout>
 )
 
