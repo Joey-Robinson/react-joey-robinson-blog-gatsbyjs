6b186b0da7914cdc64512870c0555beb97bc6257
Joey Robinson <theamazingness@gmail.com>
2019-06-27 18:08:59 -0400

    Header scroll (looks) completed. Added styling. Going to play around with media queries and grid-template-areas next, I think

--- src/components/header.js
+++ src/components/header.js
@@ -2,11 +2,27 @@ import React, { useState, useEffect } from "react"
 import DarkModeToggle from "./dark-mode"
 
 const Header = () => {
-  // const [scroll, setScroll] = useState(false)
+  const [scroll, setScroll] = useState(false)
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      // if (window.scrollY > 20) {
+      //   setScroll(true)
+      // } else {
+      //   setScroll(false)
+      // }
+      {
+        window.scrollY > 20 ? setScroll(true) : setScroll(false)
+      }
+    })
+    return () => {
+      setScroll()
+    }
+  }, [])
 
   return (
-    <header className="header">
-      <h1>From header</h1>
+    <header className={scroll ? "header scroll" : "header"}>
+      <h1>Joey Robinson</h1>
       <DarkModeToggle />
     </header>
   )
