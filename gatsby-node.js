0fd3bdf6bae97686fa4bd24ecde232296a0a2da3
Joey Robinson <theamazingness@gmail.com>
2019-07-10 18:27:32 -0400

    Just checking to see if the content from -node.js was needed.

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,17 +1,3 @@
-const { createFilePath } = require(`gatsby-source-filesystem`)
-
-exports.onCreateNode = ({ node, actions, getNode }) => {
-  const { createNodeField } = actions
-  if (node.internal.type === `allContentfulBlogPost`) {
-    const value = createFilePath({ node, getNode })
-    createNodeField({
-      name: `slug`,
-      node,
-      value,
-    })
-  }
-}
-
 exports.createPages = async ({ actions, graphql, reporter }) => {
   const result = await graphql(`
     {
